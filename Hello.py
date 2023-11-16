042674e2904860c2364180eb060a35aabe7ecf24
jhonjasanchez <jhonjasanchez@gmail.com>
2023-11-16 00:44:33 +0000

    Modificación para el fondo.

--- Hello.py
+++ Hello.py
@@ -7,9 +7,29 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import base64
 
 LOGGER = get_logger(__name__)
 
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/jpg;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
 def run():
     st.set_page_config(
@@ -17,17 +37,7 @@ def run():
         page_icon=":mushroom:",
     )
 
-    css = """
-    <style>
-        body {
-            background-image: url('https://images6.alphacoders.com/877/877243.jpg');
-            background-size: cover;
-        }
-    </style>
-    """
-
-    # Display the CSS in the Streamlit app
-    st.markdown(css, unsafe_allow_html=True)
+    set_png_as_page_bg('/workspaces/fungis-classification/background.jpg')
 
     st.write("# Clasificación de Hongos! :mushroom:")
 
