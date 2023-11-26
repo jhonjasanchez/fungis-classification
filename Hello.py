11e1211d351e69ec14019f09bc658fe5c3db881d
jhonjasanchez <jhonjasanchez@gmail.com>
2023-11-26 15:42:14 +0000

    ajuste para verificar ruta.

--- Hello.py
+++ Hello.py
@@ -49,7 +49,7 @@ def run():
 
     if st.button("Clasificar !!!"):
       # Ruta al modelo entrenado
-      model_path = "yolov5_model.pkl"
+      model_path = "yolov5_modelf.pkl"
 
       # Cargar el modelo
       try:
